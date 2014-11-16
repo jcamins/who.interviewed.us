6cf10336bd9370a33ec049de9e89ad4a4c604aea
Jared Camins-Esakov <jcamins@cpbibliography.com>
2014-11-16 10:10:00 -0500

    Add a typeahead

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'apptrack.services'
+    'apptrack.services',
+    'siyfion.sfTypeahead'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -13,6 +13,29 @@ app.controller('MainCtrl', ['$scope', '$resource', 'JobApp', function ($scope, $
     JobApp.query(function (jobapps) {
         $scope.jobapps = jobapps;
     });
+    var recruitercompanies = new Bloodhound({
+        datumTokenizer: function (d) { if (d) { return Bloodhound.tokenizers.whitespace(d.value); } },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: {
+            url: 'applications.json',
+            ttl: 1000,
+            filter: function (data) {
+                var sugs = [ ];
+                var seen = { };
+                data.forEach(function (ja) {
+                    if (ja.recruiter && ja.recruiter.company && !seen[ja.recruiter.company]) {
+                        seen[ja.recruiter.company] = true;
+                        sugs.push({ value: ja.recruiter.company});
+                    }
+                });
+                return sugs;
+            }
+        }
+    });
+    recruitercompanies.initialize();
+    $scope.recruitercompanies = { 
+        source: recruitercompanies.ttAdapter()
+    };
 }]);
 
 app.controller('AboutCtrl', ['$scope', function ($scope) {
