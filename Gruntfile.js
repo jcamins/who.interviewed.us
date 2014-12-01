214bb96e3383a4e4bde6aedfa7a0a492d9cff0e8
Jared Camins-Esakov <jcamins@cpbibliography.com>
2014-11-30 22:12:02 -0500

    Remove CSS that is being built into main.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,7 +171,8 @@ module.exports = function (grunt) {
     wiredep: {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
-        ignorePath:  /\.\.\//
+        ignorePath:  /\.\.\//,
+        exclude: [ 'bootstrap.css', 'font-awesome.css' ]
       }
     },
 
